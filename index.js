4f70616fba2e3a9671237808992660eaf9eaf494
cocktailpeanut <cocktailpeanuts@proton.me>
2023-01-29 19:52:38 -0500

    display version

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const packagejson = require('./package.json')
 const Breadmachine = require('breadmachine');
 (async () => {
   const bread = new Breadmachine()
   await bread.init({
+    version: packagejson.version,
     config: path.resolve(__dirname, "breadboard.yaml"),
     releases: {
       feed: "https://github.com/cocktailpeanut/breadboard-web/releases.atom",
